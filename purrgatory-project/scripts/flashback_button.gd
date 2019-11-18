be1ea4603fd2e593de9ee9bfdedbaeccf7b76c16
Darvin <darvinheo@gmail.com>
2019-11-17 23:53:21 -0500

    flashbacks code

---
+++ purrgatory-project/scripts/flashback_button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func highlight_text(on):
+	if on:
+		get_parent().modulate = Color(0.75, 0.75, 0.75, get_parent().modulate.a)
+	else:
+		get_parent().modulate = Color(1, 1, 1, get_parent().modulate.a)
+		
+func show_custom():
+	var mouse = get_global_mouse_position()
+	var rect = get_rect()
+	highlight_text(get_rect().has_point(mouse))
+	get_parent().show()
--- purrgatory-project/scripts/rooms/flashback_oliver.gd
+++ purrgatory-project/scripts/rooms/flashback_oliver.gd
@@ -1,6 +1,20 @@
 extends "state_handler_template.gd"
 
-var n = 0
+var last_editable_text = null
+var text_n = -1
+var choice_n = 0
+var choice_log = {}
+var text_dependencies = {
+	4: 3
+}
+
+func _ready():
+	for text in $text_container.get_children():
+		if text is RichTextLabel:
+			text.modulate.a = 0
+	$next_cover.show()
+	$next_button.hide()
+	$wake_up_cover.hide()
 
 func update_state(state):
 	.update_state(state)
@@ -8,22 +22,88 @@ func update_state(state):
 		emit_signal('change_room', 'commons1')
 	
 func next():
-	if n < $text_container.get_children().size():
-		var current = $text_container.get_child(n)
-		current.get_node('AnimationPlayer').play("Fadein")
-		n += 1
+	# if the previous line of text was a choice, turn off the button so it doesn't cause highlighting
+	var old = $text_container.get_child(text_n)
+	if old and old.name[0] == 'c':
+		var old_choice = old.get_node('choice_container').get_child(choice_n)
+		old_choice.get_node('Button').hide()
 		
+		# also get rid of the underline and store the choice for later
+		old_choice.set_bbcode(old_choice.get_bbcode().lstrip('[u]').rstrip('[/u].') + '.')
+		choice_log[text_n] = choice_n
+	
+	# else if the previous line of text had a lineedit, format it correctly
+	if old and old.find_node('LineEdit'):
+		var lineedit = old.find_node('LineEdit')
+		lineedit.get_parent().set_bbcode(last_editable_text % lineedit.text)
+		lineedit.get_parent().get_node('underline').hide()
+		lineedit.hide()
+	
+	# try getting the next line of text
+	text_n += 1
+	var current = $text_container.get_child(text_n)
+	# if there is one...
+	if current is RichTextLabel:
+		# fade in the new text
+		current.get_node('AnimationPlayer').play("Fadein")
+		
+		# if it's a regular text...
 		if current.name[0] == 't':
 			$next_cover.show()
 			$next_button.hide()
+			
+			# if it's a variable text, show the correct line based on a previous choice
+			if text_dependencies.has(text_n):
+				var var_text = current.get_child(choice_log[text_dependencies[text_n]] + 1)
+				var_text.show()
+				
+				# check if there's a lineedit WITHIN the variable text
+				if var_text.name[0] == 'e':
+					last_editable_text = var_text.get_bbcode()
+					var_text.set_bbcode(last_editable_text % '                              ')
+					$next_cover.hide()
+					show_next_button()
+		
+		# if it's a lineedit...
+		if current.name[0] == 'e':
+			last_editable_text = current.get_bbcode()
+			current.set_bbcode(last_editable_text % '                              ')
+			$next_cover.hide()
+			show_next_button()
+			
+		# if it's a choice...
 		elif current.name[0] == 'c':
 			$next_cover.hide()
-			$next_timer.start()
+			choice_n = 0
+			
+			# show the default choice
+			var default_choice = current.get_node('choice_container').get_child(0).show()
 			
 	else: 
-		for child in $text_container.get_children():
-			child.get_node('AnimationPlayer').play_backwards("Fadein")
+		$next_button.hide()
+		$next_cover.hide()
+		
+		# if the last line of text was interactable, then let them look at it for a while
+		if old and (old.name[0] == 'c' or old.find_node('LineEdit')):
+			$next_cover.show()
+		
+		# otherwise fade out all the text and enable the "wake up" cover
+		else:
+			$text_container/AnimationPlayer.play_backwards('Fadein')
 			$wake_up_cover.show()
+
+func cycle_choice():
+	var choice_container = $text_container.get_child(text_n).get_node('choice_container')
+	choice_container.get_child(choice_n).hide()
+	
+	choice_n += 1
+	if choice_n >= choice_container.get_children().size():
+		choice_n %= choice_container.get_children().size()
+		
+	choice_container.get_child(choice_n).get_node('Button').show_custom()
+	
+	if not $next_button.visible:
+		show_next_button()
 
 func show_next_button():
 	$next_button.modulate.a = 0
