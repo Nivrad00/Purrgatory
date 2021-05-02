4282a6bd2b3b2d9adf0d754237f283b97c593393
Nivrad00 <darvinheo@gmail.com>
2021-05-01 20:27:14 -0400

    finished good credits

--- purrgatory-project/scripts/rooms/bad_credits.gd
+++ purrgatory-project/scripts/rooms/bad_credits.gd
@@ -1,5 +1,15 @@
 extends 'state_handler_template.gd'
 
+func show_rotating_cat():
+	get_node('../../../rotating_cat_container').rect_position = Vector2(289, 180)
+	get_node('../../../rotating_cat_container').rect_scale = Vector2(0.6, 0.6)
+	get_node('../../../rotating_cat_container').modulate.a = 1
+	
+func hide_rotating_cat():
+	get_node('../../../rotating_cat_container').rect_position = Vector2(0, 0)
+	get_node('../../../rotating_cat_container').rect_scale = Vector2(0.001, 0.001)
+	get_node('../../../rotating_cat_container').modulate.a = 0.01
+		
 func disable_ui():
 	# in case it wasn't handled by the previous room
 	get_node('../../../../..').disable_ui()
@@ -7,7 +17,7 @@ func disable_ui():
 func _on_back_pressed():
 	get_node('../../../../..').return_to_main()
 
-func skip_credits():
+func skip_bad_credits():
 	$AnimationPlayer.stop()
 	get_node('1').hide()
 	get_node('2').hide()
@@ -17,9 +27,24 @@ func skip_credits():
 	get_node('6').hide()
 	get_node('7').hide()
 	$TextureRect.hide()
-	$bg.hide()
 	$skip.hide()
 	
 	$last.show()
 	$last/back.show()
 	$last/Label2.show()
+
+func skip_good_credits():
+	$AnimationPlayer.stop()
+	get_node('1').hide()
+	get_node('2').hide()
+	get_node('3').hide()
+	get_node('4').hide()
+	get_node('5').hide()
+	get_node('6').hide()
+	get_node('7').hide()
+	get_node('8').hide()
+	$skip.hide()
+	
+	$last.show()
+	
+	hide_rotating_cat()
